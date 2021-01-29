a6e465d4448cd2bcd55e3e5c2396d6b818b7462a
Neha Choudhary <nehachoudhary0814@gmail.com>
2021-01-29 15:46:15 +1300

    changes css file

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './notestyle.css';
-import Logo from './Images/Logo.png';
+
 
 const Header = () =>{
 return (
